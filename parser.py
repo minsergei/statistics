c652a0802da852dcb36114a1ce15f6f853fb7ec7
min <min@example.com>
2025-04-30 14:15:22 +0300

    develop5

--- parser.py
+++ parser.py
@@ -1,5 +1,7 @@
 import csv
 import os
+import re
+import itertools
 
 
 def open_csv(filename):
@@ -53,20 +55,29 @@ def parser_data_of_time(data):
 data_for_parser = open_csv("spartak.csv")
 
 total_data = " ".join(data_for_parser[1][3:4])
-
-print(total_data)
-# for i in data_for_parser[1][3:]:
-    # total_data = " ".join(i)
-    # print(total_data)
-    # print(i)
-
+# print(total_data.split())
+all_list_only_sum = []
+all_list_without_summ = []
+kpp_dict = {}
+for i in data_for_parser[1][4:]:
+    str = re.split(r"\s\s+", i)
+    if "ВСЕГО" in str[1]:
+        all_list_only_sum.append(str)
+    else:
+        all_list_without_summ.append(str)
+for name, group in itertools.groupby(all_list_without_summ, lambda x: x[0]):
+    kpp_group = []
+    for i in group:
+        kpp_group.append(i[1:])
+    kpp_dict[name] = kpp_group
 # dictionary = {
 #     'G1': {
 #         'G1_1': 15, 'G1_2': 44, 'G1_3': 145}}
-# print(dictionary.values())
-# for i in dictionary.values():
-#     print(i)
-#     for i in i.values():
+print(kpp_dict.keys())
+# print(all_dict)
+# for i in all_dict.values():
+#     # print(i)
+#     for i in i:
 #         print(i)
 
 
